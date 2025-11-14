b787a83da73b1060fee37c36ed0580cdbaa2227b
rbgranda-developer <rbgranda@est.istg.edu.ec>
2025-11-13 20:46:03 -0500

    cambios

--- app/src/main/java/com/example/app_movies/LoginScreen.kt
+++ app/src/main/java/com/example/app_movies/LoginScreen.kt
@@ -11,59 +11,55 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.app_movies.R // Asegúrate de que esta sea la ruta correcta a tu R
+import com.example.app_movies.R
 
 @Composable
 fun LoginScreen() {
-    var username by remember { mutableStateOf("") }
-    var password by remember { mutableStateOf("") }
+
+    var usuario by remember { mutableStateOf("") }
+    var contrasena by remember { mutableStateOf("") }
 
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(24.dp), // Aumentamos un poco el padding
+            .padding(24.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        // Logo
         Image(
-            // Carga tu archivo login.png
             painter = painterResource(id = R.drawable.login),
             contentDescription = "Logo de la aplicación",
-            modifier = Modifier.size(150.dp) // Tamaño del logo un poco más grande
+            modifier = Modifier.size(150.dp)
         )
 
-        Spacer(modifier = Modifier.height(48.dp)) // Espacio grande
+        Spacer(modifier = Modifier.height(48.dp))
 
-        // Campo Username
         OutlinedTextField(
-            value = username,
-            onValueChange = { username = it },
-            label = { Text("Username") },
-            modifier = Modifier.fillMaxWidth() // Ocupa todo el ancho
+            value = usuario,
+            onValueChange = { usuario = it },
+            label = { Text("Usuario") },
+            modifier = Modifier.fillMaxWidth()
         )
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        // Campo Password
         OutlinedTextField(
-            value = password,
-            onValueChange = { password = it },
-            label = { Text("Password") },
-            modifier = Modifier.fillMaxWidth() // Ocupa todo el ancho
+            value = contrasena,
+            onValueChange = { contrasena = it },
+            label = { Text("Contraseña") },
+            modifier = Modifier.fillMaxWidth()
         )
 
         Spacer(modifier = Modifier.height(24.dp))
 
-        // Botón Login
+
         Button(
             onClick = {
-                // Aquí va la lógica de autenticación (ej. Firebase o API)
-                println("Iniciando sesión con: $username / $password")
+                println("Iniciando sesión con: $usuario / $contrasena")
             },
             modifier = Modifier.fillMaxWidth()
         ) {
-            Text("Login")
+            Text("Iniciar Sesión")
         }
     }
 }
